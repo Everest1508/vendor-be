d11cc1f9c49d182f1c27dbad717aeb9f2095e30a
Everest1508 <riteshmahale15@gmail.com>
2024-05-10 02:09:23 +0530

    cc

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import VendorListCreateAPIView,VendorDetailView,PurchaseOrderListCreateView,PurchaseOrderDetailView,VendorPerformanceViewSet
+from .views import VendorListCreateAPIView,VendorDetailView,PurchaseOrderListCreateView,PurchaseOrderDetailView,VendorPerformanceViewSet,AssignVendorToPOAPIView
 urlpatterns = [
-    path("vendors/",VendorListCreateAPIView.as_view(),name="list-create-vendors"),
-    path("vendors/<int:vendor_id>/",VendorDetailView.as_view(),name="detail-vendors"),
+    path("vendors/",VendorListCreateAPIView.as_view()),
+    path("vendors/<int:vendor_id>/",VendorDetailView.as_view()),
     path("purchase-orders/", PurchaseOrderListCreateView.as_view()),
+    path("purchase-orders/assign/", AssignVendorToPOAPIView.as_view()),
     path("purchase-orders/<int:po_id>/", PurchaseOrderDetailView.as_view()),
-    path("purchase_orders/<int:pk>/acknowledge/", VendorPerformanceViewSet.as_view({'post': 'update_acknowledgment'}), name='acknowledge-po'),
-    path("vendors/<int:pk>/performance/", VendorPerformanceViewSet.as_view({'get': 'retrieve'}), name='vendor-performance'),
+    path("purchase_orders/<int:pk>/acknowledge/", VendorPerformanceViewSet.as_view({'post': 'update_acknowledgment'})),
+    path("vendors/<int:pk>/performance/", VendorPerformanceViewSet.as_view({'get': 'retrieve'})),
 ]
