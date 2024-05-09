8e5a57135336aa3deca91c4dc186853e084d5820
Everest1508 <riteshmahale15@gmail.com>
2024-05-10 01:59:22 +0530

    cc

--- vendor/views.py
+++ vendor/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from .models import Vendor,PurchaseOrder,HistoricalPerformance
-from .serializers import VendorSerializer,VendorPerformanceSerializer,PurchaseOrderSerializer
+from .serializers import VendorSerializer,VendorPerformanceSerializer,PurchaseOrderSerializer,AssignVendorToPOSerializer
 
 class VendorListCreateAPIView(APIView):
     def get(self,request):
