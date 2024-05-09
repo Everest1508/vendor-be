2c428ae2e0ae9ccd47f9ff07f48f1abd6e43b351
Everest1508 <riteshmahale15@gmail.com>
2024-05-10 00:52:20 +0530

    cc

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import VendorListCreateAPIView,VendorDetailView,PurchaseOrderListCreateView,PurchaseOrderDetailView
+from .views import VendorListCreateAPIView,VendorDetailView,PurchaseOrderListCreateView,PurchaseOrderDetailView,VendorPerformanceViewSet
 urlpatterns = [
     path("vendors/",VendorListCreateAPIView.as_view(),name="list-create-vendors"),
     path("vendors/<int:vendor_id>/",VendorDetailView.as_view(),name="detail-vendors"),
     path("purchase-orders/", PurchaseOrderListCreateView.as_view()),
     path("purchase-orders/<int:po_id>/", PurchaseOrderDetailView.as_view()),
+    path('api/purchase_orders/<int:pk>/acknowledge/', VendorPerformanceViewSet.as_view({'post': 'update_acknowledgment'}), name='acknowledge-po'),
+    path('api/vendors/<int:pk>/performance/', VendorPerformanceViewSet.as_view({'get': 'retrieve'}), name='vendor-performance'),
 ]
